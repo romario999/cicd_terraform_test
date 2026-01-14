60f845d08dc62dbf878d516f871f62d874af8eaa
Your Name <you@example.com>
2026-01-14 10:12:50 +0000

    444

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "7vqc6vtc-bucket-cicd-tf-20260114095224"
+    bucket = "7vqc6vtc-bucket-cicd-tf-20260114101128"
     key    = "terraform/terraform.tfstate"
     region = "us-east-1"
   }
