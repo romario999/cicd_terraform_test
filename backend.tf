b9b37311f9abdd36d8bcc9a82fc64051535ae7d6
Your Name <you@example.com>
2026-01-21 08:47:43 +0000

    jenkins_test

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "7vqc6vtc-bucket-cicd-tf-20260116083120"
+    bucket = "7vqc6vtc-bucket-cicd-tf-20260121084602"
     key    = "terraform/terraform.tfstate"
     region = "us-east-1"
   }
